15e4b3bee25c916d2d8379e3644eac5b6d6c73a6
Bitnix <bitnix@riseup.net>
2020-02-08 20:33:29 +0000

    Added StaticFilterTest

---
+++ src/Flogger/Filter/StaticFilter.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types=1);
+
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ */
+
+namespace Bitnix\Log\Flogger\Filter;
+
+use Bitnix\Log\Flogger\Filter;
+
+/**
+ * @version 0.1.0
+ */
+final class StaticFilter implements Filter {
+
+    /**
+     * @var bool
+     */
+    private bool $accept;
+
+    /**
+     * @param bool $accept
+     */
+    public function __construct(bool $accept = false) {
+        $this->accept = $accept;
+    }
+
+    /**
+     * @param string $haystack
+     * @return bool
+     */
+    public function accept(string $haystack) : bool {
+        return $this->accept;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString() : string {
+        return self::CLASS;
+    }
+}
